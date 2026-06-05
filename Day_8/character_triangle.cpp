509a98499a234009930f9c5d20aec08dcccb3486
Somgupta82 <guptasom462@gmail.com>
2026-06-05 17:06:51 +0530

    Add Day_8 patterns

---
+++ Day_8/character_triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number  ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        char a = 'A';
+        for (int j = 1; j <= i; j++)
+        {
+            cout << a;
+            a = a + 1;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
