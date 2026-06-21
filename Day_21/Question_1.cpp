92e9608b3b121f57ac57c4b66012cefd2c290708
Somgupta82 <guptasom462@gmail.com>
2026-06-21 23:08:25 +0530

    Add Day_21

---
+++ Day_21/Question_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[100];
+    cin >> str;
+    int length = 0;
+    while (str[length] != '\0')
+        length++;
+    {
+        cout << "length=" << length;
+    }
+    return 0;
+}
